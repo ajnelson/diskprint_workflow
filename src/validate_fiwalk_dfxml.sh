2cc1b3705c43be8b337039c3f79a7cad321d2746
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-09-05 18:17:07 -0400

    Change output quietting

--- src/validate_fiwalk_dfxml.sh
+++ src/validate_fiwalk_dfxml.sh
@@ -6,9 +6,9 @@ path_to_dfxml="${path_to_fiwalk_dir}/fiout.dfxml"
 path_to_ao_dfxml="${path_to_fiwalk_dir}/fiout-alloc-only.dfxml"
 
 echo "Validating: $path_to_dfxml" >&2
-xmllint --schema "${dwf_dfxml_schema}" "${path_to_dfxml}" >/dev/null
+xmllint --noout --schema "${dwf_dfxml_schema}" "${path_to_dfxml}"
 echo $? >"${output_dir}/fiout.dfxml.status.log"
 
 echo "Validating: $path_to_ao_dfxml" >&2
-xmllint --schema "${dwf_dfxml_schema}" "${path_to_ao_dfxml}" >/dev/null
+xmllint --noout --schema "${dwf_dfxml_schema}" "${path_to_ao_dfxml}"
 echo $? >"${output_dir}/fiout-alloc-only.dfxml.status.log"
