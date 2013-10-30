bb06d92010d1c633b4face5031142437716c80e2
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-10-30 14:49:32 -0400

    Make more use of env. Pythons

--- src/make_sequence_list.sh
+++ src/make_sequence_list.sh
@@ -5,7 +5,10 @@
 #One-liner c/o http://stackoverflow.com/a/246128/1207160
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+#Define Pythons
+source "${script_dir}/_pick_pythons.sh"
+
 final_tarball_path="$1"
 outfile="$2/sequence_tarballs.txt"
 
-"$script_dir/make_sequence_list.py" --config="$DIFFER_CONFIG" "$final_tarball_path" >"$outfile"
+"$PYTHON2" "$script_dir/make_sequence_list.py" --config="$DIFFER_CONFIG" "$final_tarball_path" >"$outfile"
