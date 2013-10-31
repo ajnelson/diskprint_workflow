d67a9b30dec429af579aca8a575531111a201220
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-10-31 10:56:25 -0400

    Adjust parallel jobs allocation

--- src/do_difference_workflow.sh
+++ src/do_difference_workflow.sh
@@ -233,8 +233,8 @@ if [ $parallel_all -eq 1 ]; then
     parg_report_pidlog="--report-pidlog"
   fi
   ./sliceprocessor.py --config "$DIFFER_CONFIG" --tails_only | \
-    parallel --quote -j$num_jobs \
-      "$0" "$parg_report_pidlog" "$parg_re_export" --cleanup ignore -j 1 --config "$DIFFER_CONFIG" --quiet {} "$@"
+    parallel --quote -j1 \
+      "$0" "$parg_report_pidlog" "$parg_re_export" --cleanup ignore -j $num_jobs --config "$DIFFER_CONFIG" --quiet {} "$@"
   exit 0
 fi
 
