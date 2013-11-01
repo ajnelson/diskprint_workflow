91df16f1204417eba4ce1dd11b6b722196ed7504
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-11-01 10:36:38 -0400

    Correct script typo

--- src/make_sequence_deltas.sh
+++ src/make_sequence_deltas.sh
@@ -9,7 +9,7 @@ debug=1
 
 #One-liner c/o http://stackoverflow.com/a/246128/1207160
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-$top_src_dir="${script_dir}/.."
+top_src_dir="${script_dir}/.."
 
 #Define Pythons
 source "${script_dir}/_pick_pythons.sh"
