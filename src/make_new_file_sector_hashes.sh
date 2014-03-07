d137fb185257672dfaedf0e7ac9560ba29ca0ea0
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-03-07 16:24:34 -0500

    Fix slice referencing

--- src/make_new_file_sector_hashes.sh
+++ src/make_new_file_sector_hashes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "DEBUG:make_new_file_sector_hashes.sh:\$PATH = $PATH"
+
 #One-liner c/o http://stackoverflow.com/a/246128/1207160
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
@@ -7,9 +9,10 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 source "${script_dir}/_pick_pythons.sh"
 
 dwf_output_dir="$2"
+source "$script_dir/_results_sequences.sh"
 
 input_dfxml="${dwf_output_dir}/../make_differential_dfxml_prior.sh/deltas.dfxml"
-input_disk_image="${dwf_output_dir}/../invoke_vmdk_to_E01.sh/out.E01"
+input_disk_image="${dwf_tarball_results_dirs[$dwf_tarball_results_dirs_index_current]}/invoke_vmdk_to_E01.sh/out.E01"
 
 "$PYTHON3" "${script_dir}/hash_sectors.py" \
   --debug \
