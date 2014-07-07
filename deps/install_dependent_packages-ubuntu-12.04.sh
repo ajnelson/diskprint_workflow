e3a09b17f5bbd3e9ba4d18e3f5a20aed4965825c
Jim Jones <jjonesu@gmu.edu>
2014-07-07 16:16:00 -0400

    Add Ubuntu package list

---
+++ deps/install_dependent_packages-ubuntu-12.04.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#One-liner c/o http://stackoverflow.com/a/246128/1207160
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+#Note that this included script sets -e and -x, so errors will be caught appropriately.
+source "${script_dir}/regxml_extractor.git/deps/install_dependent_packages-ubuntu-12.04.sh"
+
+sudo apt-get install \
+  coreutils \
+  libewf-dev \
+  libewf1 \
+  ewf-tools \
+  parallel \
+  python-psycopg2 \
+  python3-psycopg2 \
+  python2.7 \
+  python3 \
+  qemu
+
+echo "Done."
