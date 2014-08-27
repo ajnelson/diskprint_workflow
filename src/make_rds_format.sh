e20a5de2a432cfad817d19193f2eb1c578db820d
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-08-27 15:45:43 -0400

    Fix variable reference

--- src/make_rds_format.sh
+++ src/make_rds_format.sh
@@ -9,7 +9,7 @@ source "${script_dir}/_pick_pythons.sh"
 dwf_output_dir="${2}"
 source "$script_dir/_results_sequences.sh"
 
-dfxml="${output_dir}/../make_differential_dfxml_prior.sh/deltas.dfxml"
+dfxml="${dwf_output_dir}/../make_differential_dfxml_prior.sh/deltas.dfxml"
 diskimage="${dwf_tarball_results_dirs[$dwf_tarball_results_dirs_index_current]}/invoke_vmdk_to_E01.sh/out.E01"
 
 "$PYTHON3" "${script_dir}/make_rds_format.py" "$dfxml" "$diskimage" "${dwf_output_dir}/NSRLFile.txt"
