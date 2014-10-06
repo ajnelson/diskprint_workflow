00ac04861efc9af176f4720ac6e0e5ccd3e9778a
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-10-05 22:49:52 -0400

    Revert merge artifacts
    Some changes made to 'unstable' were inappropriately carried forward
    through the merge.  Revert them to
    f871c5ebd1af7359b142fedcb7e3a4b6381fc937.

--- src/make_cybox_format.sh
+++ src/make_cybox_format.sh
@@ -8,15 +8,8 @@ set -e
 #Define Pythons
 source "${script_dir}/_pick_pythons.sh"
 
-dwf_output_dir="${2}"
-source "$script_dir/_results_sequences.sh"
+output_dir="${2}"
 
-#Skip for beginning of sequence (there is no prior for differences)
-if [ $dwf_tarball_results_dirs_index_current -eq 0 ]; then
-  echo "INFO:$(basename $0):Skipping differential analysis step on baseline image.  (No pre-baseline data in sequence.)" >&2
-  exit 0
-fi
+rdsout="${output_dir}/../make_rds_format.sh/NSRLFile.txt"
 
-rdsout="${dwf_output_dir}/../make_rds_format.sh/NSRLFile.txt"
-
-"$PYTHON2" "${script_dir}/cyboxFileObj.py" "$rdsout" > "${dwf_output_dir}/FileObjs.xml"
+"$PYTHON2" "${script_dir}/cyboxFileObj.py" "$rdsout" > "${output_dir}/FileObjs.xml"
--- src/make_new_file_sector_hashes.sh
+++ src/make_new_file_sector_hashes.sh
@@ -11,12 +11,6 @@ source "${script_dir}/_pick_pythons.sh"
 node_id1="$1"
 dwf_output_dir="$2"
 
-#Skip for beginning of sequence (there is no prior for differences)
-if [ $dwf_tarball_results_dirs_index_current -eq 0 ]; then
-  echo "INFO:$(basename $0):Skipping differential analysis step on baseline image.  (No pre-baseline data in sequence.)" >&2
-  exit 0
-fi
-
 input_dfxml="${dwf_output_dir}/../make_differential_dfxml_prior.sh/deltas.dfxml"
 input_disk_image="${dwf_all_results_root}/by_node/${node_id1}/link_disk.sh/disk0.E01"
 
