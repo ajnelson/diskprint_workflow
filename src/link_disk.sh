b61d16a47f6f04dd78f4cfe3366547936c5a34b0
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-10-18 00:47:43 -0400

    Catch errors in fetch_node_data.py

--- src/link_disk.sh
+++ src/link_disk.sh
@@ -10,12 +10,13 @@ source "${script_dir}/_pick_pythons.sh"
 node_id="$1"
 dwf_output_dir="$2"
 
-"$PYTHON3" "${script_dir}/fetch_node_data.py" --debug --config="$DIFFER_CONFIG" disk "$node_id" | while read disk_image_path; do
+"$PYTHON3" "${script_dir}/fetch_node_data.py" --debug --config="$DIFFER_CONFIG" disk "$node_id" > "${dwf_output_dir}/disk_image_paths.txt"
+
+while read disk_image_path; do
   if [ ! -r "$disk_image_path" ]; then
     echo "ERROR:$(basename $0):Disk image path retrieved from database is not a readable file." >&2
     echo "DEBUG:$(basename $0):disk_image_path = ${disk_image_path}." >&2
     exit 1
   fi
   ln -s "$disk_image_path" "${dwf_output_dir}/"
-done
-
+done < "${dwf_output_dir}/disk_image_paths.txt"
